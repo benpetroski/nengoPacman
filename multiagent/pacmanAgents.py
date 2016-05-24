53c01bebcc0452439b03531b17722e7861290f7c
benpetroski <ben@benpetroski.com>
2016-05-24 14:36:19 -0700

    added nengo agent and plotting stuff

--- multiagent/pacmanAgents.py
+++ multiagent/pacmanAgents.py
@@ -18,6 +18,22 @@ import random
 import game
 import util
 
+
+# NENGO & NUMPY STUFF
+import nengo                                # neural network simulation library
+from nengo.utils.functions import piecewise # useful for defining a piecewise function of time
+import numpy as np                          # a scientific computing library
+import matplotlib
+matplotlib.use("TkAgg")
+from matplotlib import pyplot as plt
+# from matplotlib import pyplot as plt        # a plotting library
+# from mpl_toolkits.mplot3d import Axes3D     # necessary for generating 3D plots
+# plt.rcParams['axes.labelsize'] = 20         # set default plot axes label font size
+# plt.rcParams['axes.titlesize'] = 24         # set default plot title font size
+# plt.rcParams['legend.fontsize'] = 18        # set default plot legend font size
+# make plots inline in the notebook instead of creating a new window
+
+
 class LeftTurnAgent(game.Agent):
     "An agent that turns left at every opportunity"
 
@@ -47,6 +63,50 @@ class GreedyAgent(Agent):
         bestScore = max(scored)[0]
         bestActions = [pair[1] for pair in scored if pair[0] == bestScore]
         return random.choice(bestActions)
+
+class NengoAgent(Agent):
+    def __init__(self):
+        T = 1.               # duration of simulation
+        tau_ens_probe = .01  # Synapse param when creating Probes of Ensembles
+        in_fun = lambda t: np.sin(2*np.pi*t)  # input function to your network
+        N = 100  # Number of neurons in each Ensemble
+
+        self.network = nengo.Network()
+        with self.network:
+            ensemble = nengo.Ensemble(N, dimensions=1)
+            stimulator = nengo.Node(in_fun)
+            nengo.Connection(stimulator, ensemble)
+            ensemble_two = nengo.Ensemble(N, dimensions=1)
+            def square(x):
+                return np.square(x)
+            nengo.Connection(ensemble, ensemble_two, function=square)
+            self.probe = nengo.Probe(ensemble_two, synapse=tau_ens_probe)
+            self.probe_in = nengo.Probe(ensemble, synapse=tau_ens_probe)
+
+        self.sim = nengo.Simulator(self.network)
+        self.sim.run(T)
+        input_ens, A = nengo.utils.ensemble.tuning_curves(ensemble, self.sim)
+
+
+    def getAction(self, state):
+        self.t = self.sim.trange()
+        plt.ion()
+        plt.plot(self.t, self.sim.data[self.probe])
+        plt.plot(self.t, self.sim.data[self.probe_in])
+        plt.plot(self.t, np.square(self.sim.data[self.probe_in]))
+        plt.draw()
+        plt.show()
+
+        legal = state.getLegalPacmanActions()
+        current = state.getPacmanState().configuration.direction
+        if current == Directions.STOP: current = Directions.NORTH
+        left = Directions.RIGHT[current]
+        if left in legal: return left
+        if current in legal: return current
+        if Directions.RIGHT[left] in legal: return Directions.RIGHT[left]
+        if Directions.LEFT[current] in legal: return Directions.LEFT[current]
+        return Directions.STOP
+
 
 def scoreEvaluation(state):
     return state.getScore()
